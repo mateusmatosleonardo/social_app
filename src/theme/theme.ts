db6a04ffa4b035efdca6a526227212ef4fb156bc
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-01 12:57:21 -0300

    chore: creating global theme

---
+++ src/theme/theme.ts
@@ -0,0 +1,16 @@
+export const theme = {
+  name: 'DefaultTheme',
+  colors: {
+    WHITE: '#ffffff',
+    BLACK: '#171b2f',
+    PRIMARY: '#181d3d',
+    SECONDARY: '#2d325a',
+    GRAY: '#82859c',
+    RED: '#f65364',
+  },
+  fonts: {
+    Rubik_Regular: 'Rubik_400Regular',
+    Rubik_Medium: 'Rubik_500Medium',
+    Rubik_Bold: 'Rubik_700Bold',
+  },
+};
